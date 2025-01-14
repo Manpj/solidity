862aaee1cb54299ecfb36a3395c4be89efd96c65
Manpj <1508490638@qq.com>
2025-01-14 22:39:45 +0800

    hello contract
    first commit

---
+++ hello/test/Hello.ts
@@ -0,0 +1,17 @@
+import "@nomicfoundation/hardhat-ethers";
+import {ethers} from "hardhat";
+import {expect} from "chai";
+
+describe("HelloWorld",function (){
+  it("should say hello",async function (){
+    //安装合约setup
+    //import contract
+    //test action
+    
+    const HW=await ethers.getContractFactory("HelloWorld");
+    const hw=await HW.deploy();
+    await hw.waitForDeployment();
+
+    expect((await hw.hello()).to.equal("Hello, World"));
+  });
+});
