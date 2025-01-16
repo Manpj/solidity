b68814cffc2c23a6e1b19f96e710ebf7f2806f83
Manpj <1508490638@qq.com>
2025-01-16 18:07:57 +0800

    reentry attack  test
    重入攻击测试

---
+++ ReentryAttack/README.md
@@ -0,0 +1,19 @@
+# 重入攻击演示例子
+
+```shell
+npx hardhat help
+npx hardhat test
+REPORT_GAS=true npx hardhat test
+npx hardhat node
+npx hardhat ignition deploy ./ignition/modules/Lock.ts
+npx hardhat compile
+```
+
+
+## 创建项目：
+```shell
+1、创建文件夹
+2、yarn init -y
+3、yarn add -D hardhat
+4、npx hardhat
+```
---
+++ ReentryAttack/contracts/ReentrancyVulnerable.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.28;
+
+// Uncomment this line to use console.log
+// import "hardhat/console.sol";
+
+contract ReentrancyVulnerable {
+    
+    mapping(address=>uint) public balances;
+
+    constructor(){
+        balances[msg.sender]=1000 ether;
+    }
+
+    function withdraw(uint amount) external {
+        //验证余额
+        require(balances[msg.sender]>amount,"balance is not enough.");
+        //转账
+        (bool success,)=msg.sender.call{value:amount}("");
+        require(success,"transfer failed.");
+        //减掉取出的数量
+        balances[msg.sender]-=amount;
+    }
+
+    //获取余额,合约部署的时候要存入1000ether
+    function getBalance() external view returns(uint){
+        return balances[msg.sender];
+    }
+
+}
